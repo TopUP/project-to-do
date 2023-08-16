a6f73b8efa1982091bb1fc189b620887b044e4a4
TopUP <trade@topup.loc>
2023-08-16 10:55:17 +0300

    Функционал добавления

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,24 @@
   <div class="app-wrapper">
     <div class="title has-text-centered">Project-To-Do</div>
 
-    <div class="field is-grouped">
-      <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Add todo">
-      </p>
-      <p class="control">
-        <a class="button is-info">
-          Add
-        </a>
-      </p>
-    </div>
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped">
+        <p class="control is-expanded">
+          <input class="input" type="text" v-model="newTodoContent" placeholder="Add todo">
+        </p>
+        <p class="control">
+          <button class="button is-info" :disabled="!newTodoContent">Add</button>
+        </p>
+      </div>
+    </form>
 
-    <div class="card mb-5" v-for="i in 3">
-      <div class="card-content">
+    <div class="card mb-5" v-for="todo in todos" :key="todo.id">
+      <div class="card-content" data-id="{{todo.id}}">
         <div class="content">
           <div class="columns">
-            <div class="column is-mobile is-vcentered">Lorem ipsum leo risus</div>
+            <div class="column is-mobile is-vcentered">{{ todo.content }}</div>
             <div class="column is-5 has-text-right">
-              <button class="button is-light">&check;</button>
+              <button class="button is-light" v-if="!todo.done">&check;</button>
               <button class="button is-danger ml-2">&cross;</button>
             </div>
           </div>
@@ -30,8 +30,23 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
 
+const todos           = ref([]);
+const newTodoContent  = ref('');
+
+const addTodo = function () {
+  const newTodo = {
+    id      : uuidv4(),
+    content : newTodoContent.value,
+    done    : false
+  }
+
+  todos.value.unshift(newTodo);
+  newTodoContent.value = '';
+}
 </script>
 
 <style>
